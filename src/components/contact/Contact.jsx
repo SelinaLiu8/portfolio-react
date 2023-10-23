b97c8468dc38f292b5d069215bc664aa9544f6bc
selinal <selnal@umich.edu>
2023-10-23 12:32:33 -0400

    done

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
+import {AiOutlinePhone} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
 
 const Contact = () => {
   return (
@@ -10,25 +12,30 @@ const Contact = () => {
       <div className="container contact__container">
         <div className="contact__options">
           <article className="contact__option">
-            {MdOutlineEmail}
+            <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>selinal@umich.edu</h5>
             <a href="mailto:selinal@umich.edu">Send me an Email</a>
           </article>
           <article className="contact__option">
-            {MdOutlineEmail}
+            <AiOutlinePhone className='contact__option-icon'/>
             <h4>Phone</h4>
             <h5>734-972-5295</h5>
             <a href="mailto:selinal@umich.edu">Shoot me a text</a>
           </article>
           <article className="contact__option">
-            {MdOutlineEmail}
-            <h4>Email</h4>
-            <h5>selinal@umich.edu</h5>
-            <a href="mailto:selinal@umich.edu">Send me an Email</a>
+            <AiOutlineLinkedin className='contact__option-icon'/>
+            <h4>Linkedin</h4>
+            <h5>Selina Liu</h5>
+            <a href="http://www.linkedin.com/in/selina-liu-5b40781b8">Send me a Linkedin message</a>
           </article>
         </div>
-        <form action=""></form>
+        <form   action="https://formspree.io/f/xpzelwlo" method="POST">
+          <input type="text" name='name' placeholder='Your Full Name' required/>
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="15" placeholder='Write Your Message Here'></textarea>
+          <button type='submit' className='btn btn-primary'>Send</button>
+        </form>
       </div>
     </section>
   )
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import './footer.css'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>SelinaL</a>
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href="http://www.linkedin.com/in/selina-liu-5b40781b8"><BsLinkedin/></a>
+        <a href="https://github.com/SelinaLiu8"><BsGithub/></a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; EGATOR Tutorials. All rights reserved</small>
+        <small>This website was created with inspiration from EGATOR Tutorials by EGATOR.</small>
+      </div>
+    </footer>
   )
 }
 
